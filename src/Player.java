312f9626ca2b2dcd5308399b354ff0150106e88a
huytr <m_tran80@u.pacific.edu>
2024-11-04 15:09:47 -0800

    Added Skin.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,15 @@
+import javax.swing.Timer;
+import java.awt.event.*;
+
+import acm.graphics.*;
 
 public class Player {
-
+	private GImage player;
+	private Skin skin;
+	private int posX;
+	private int posY;
+	private boolean isFacingLeft;
+	private int gravity = 2;
+    private int runSpeed = 3;
+    private int jumpVelocity = 0;
 }
