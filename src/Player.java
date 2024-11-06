cd08a5b4b17e9c748114292798047625a38b09e3
huytr <m_tran80@u.pacific.edu>
2024-11-06 14:12:24 -0800

    Added functions to Player.java

--- src/Player.java
+++ src/Player.java
@@ -4,19 +4,21 @@ import java.awt.event.*;
 import acm.graphics.*;
 
 public class Player {
+    public static final int RUNSPEED = 3;
+    public static final int GRAVITY = 3;
+    
 	private GImage player;
 	private Skin skin;
 	private int posX;
 	private int posY;
 	private boolean isFacingLeft;
-	private int gravity = 2;
-    private int runSpeed = 3;
     private int runDirection = 0;
     private int jumpVelocity = 0;
     
     public Player() {
-    	
+    	skin = new Skin();
     }
+    
     public int getPosX() {
     	return posX;
     }
@@ -25,8 +27,21 @@ public class Player {
     	return posY;
     }
     
+    public int setRunDirection(int speed) {
+    	return runDirection + speed;
+    }
+    
+    public int setJumpVelocity(int speed) {
+    	return jumpVelocity + speed;
+    }
+    
     public void move(int velocity) {
-    	player.move(runDirection, 0);
+    	runDirection = RUNSPEED;
+    	if (isFacingLeft) {
+    		player.move(-runDirection, 0);
+    	} else {
+    		player.move(runDirection, 0);
+    	}
     }
     
     public void jump(int velocity) {
