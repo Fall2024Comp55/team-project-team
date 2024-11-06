56e693aed62e1af312561a4d20380c0ffb597301
Kelly <k_yee16@u.pacific.edu>
2024-11-06 14:21:19 -0800

    Added to trap class

--- src/Trap.java
+++ src/Trap.java
@@ -1,4 +1,27 @@
+public abstract class Trap {
+ // protected GImage trap; 
+ protected int posX;
+ protected int posY;
 
-public class Trap {
-	
+ // Constructor to initialize position and trap image
+ public Trap(int posX, int posY) {           // change to public Trap(int posX, int posY, GImage trap) later
+     this.posX = posX;
+     this.posY = posY;
+     // this.trap = trap;
+ }
+
+ // Getter for posX
+ public int getPosX() {
+     return posX;
+ }
+
+ // Getter for posY
+ public int getPosY() {
+     return posY;
+ }
+
+ // Abstract method for the specific trap action
+ public abstract void action();
 }
+
+
