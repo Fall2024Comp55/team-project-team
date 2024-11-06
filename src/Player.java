6e105f5ac6356e081be70229da6e044bc0018fbf
huytr <m_tran80@u.pacific.edu>
2024-11-06 14:06:03 -0800

    Performed a bit of code cleaning

--- src/Player.java
+++ src/Player.java
@@ -11,5 +11,25 @@ public class Player {
 	private boolean isFacingLeft;
 	private int gravity = 2;
     private int runSpeed = 3;
+    private int runDirection = 0;
     private int jumpVelocity = 0;
+    
+    public Player() {
+    	
+    }
+    public int getPosX() {
+    	return posX;
+    }
+    
+    public int getPosY() {
+    	return posY;
+    }
+    
+    public void move(int velocity) {
+    	player.move(runDirection, 0);
+    }
+    
+    public void jump(int velocity) {
+    	player.move(runDirection, jumpVelocity);
+    }
 }
--- src/Space.java
+++ src/Space.java
@@ -1,39 +1,40 @@
 
 public class Space {
-private int row;
-private int col;
-
-
-public void setRow(int x) {
-	row = x;
-}
-
-public void setCol(int y) {
-	col = y;
-}
-
-public int getRow() {
-	return row;
-}
-
-public int getCol() {
-	return col;
-}
-
-public Space(int row, int col) {
-	this.row = row;
-	this.col = col;
-}
-
-//testing to see if the class worked
-public static void main(String[] args) {
-	Space one = new Space(3, 4);
-	Space two = new Space(1, 6);
-	two.setRow(two.getRow()+1);
-	two.setCol(two.getCol()-1);
-	System.out.println("one r: " + one.getRow() + ", c: " + one.getCol());
-	System.out.println("two r: " + two.getRow() + ", c: " + two.getCol());
+	
+	private int row;
+	private int col;
+	
+	
+	public void setRow(int x) {
+		row = x;
 	}
-
+	
+	public void setCol(int y) {
+		col = y;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public Space(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	//testing to see if the class worked
+	public static void main(String[] args) {
+		Space one = new Space(3, 4);
+		Space two = new Space(1, 6);
+		two.setRow(two.getRow()+1);
+		two.setCol(two.getCol()-1);
+		System.out.println("one r: " + one.getRow() + ", c: " + one.getCol());
+		System.out.println("two r: " + two.getRow() + ", c: " + two.getCol());
+		}
+	
 
 }
