51918b73b56071d214ec32836276bfb4f96e2bd8
Jonathan Villanueva <j_villanueva11@u.pacific.edu>
2024-11-06 12:03:26 -0800

    Create and setting column

--- src/Space.java
+++ src/Space.java
@@ -12,30 +12,4 @@ public void setCol(int y) {
 	col = y;
 }
 
-public Space(int row, int col) {
-	this.row = row;
-	this.col = col;
-}
-
-
-public int getRow() {
-	return row;
-}
-
-public int getCol() {
-	return col;
-}
-
-
-public static void main(String[] args) {
-	Space one = new Space(3, 4);
-	Space two = new Space(1, 6);
-	two.setRow(two.getRow()+1);
-	two.setCol(two.getCol()-1);
-	System.out.println("one r: " + one.getRow() + ", c: " + one.getCol());
-	System.out.println("two r: " + two.getRow() + ", c: " + two.getCol());
-	}
-
-
-
 }
