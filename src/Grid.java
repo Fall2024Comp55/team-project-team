635ca89253af23b0415ac4ac24717cec32047005
Jonathan Villanueva <j_villanueva11@u.pacific.edu>
2024-11-08 11:53:39 -0800

    grid can now place the wind traps

--- src/Grid.java
+++ src/Grid.java
@@ -3,6 +3,7 @@ public class Grid {
  private int numRows;
  private int numCols;
  private Space[][] grid; //2d array representing the grid
+ private int windPower = 5; //the strength of the wind
  //do a gRect to create platforms
  
  //creating a grid constructor
@@ -27,7 +28,7 @@ public class Grid {
 	 return this.numCols;
  }
  
- public Space getSpace(int row, int col) {
+ public Space getSpace(int row, int col) { 
 	 if(isValidPosition(row, col)) {
 		 return grid[row][col];
 	 }
@@ -36,14 +37,31 @@ public class Grid {
 	 }
  }
  
- public void setPushTrap(int row, int col) {
+ public void setPushTrap(int row, int col) {// creation of push traps
 	 if(isValidPosition(row,col)) {
 		Trap.PushSpring pushTrap = new Trap.PushSpring(row, col);
 		grid[row][col].setTrap(pushTrap);
 	 }
  }
  
- public void setPlatform(int row, int col) {
+ public String windDirection(int x) {// set the direction for wind to blow
+	 if( x == 0) {
+		 return "left";
+	 }
+	 else if(x == 1) {
+		 return "right";
+	 }
+	return null;
+ }
+ 
+ public void setWindTrap(int row, int col, int direction) {// creation of wind traps
+	 if(isValidPosition(row,col)) {
+		 Trap.Wind windTrap = new Trap.Wind(row, col, windDirection(direction), windPower);
+		 grid[row][col].setTrap(windTrap);
+	 }
+ }
+ 
+ public void setPlatform(int row, int col) {// creating of platforms in grid space
 	 if(isValidPosition(row,col)) {
 		 grid[row][col] = //platform, mat;
 	 }
