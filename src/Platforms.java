cc955e4f0e954bb8e13a1efb089b97254f3b0ed0
Jonathan Villanueva <j_villanueva11@u.pacific.edu>
2024-11-08 12:12:05 -0800

    Platform now specifies height and width

--- src/Platforms.java
+++ src/Platforms.java
@@ -1,4 +1,19 @@
 
 public class Platforms {
+private int width;
+private int height;
+
+public Platforms(int width, int height) {
+	this.width = width;
+	this.height = height;
+}
+
+public int getWidth() {
+	return width;
+}
+
+public int getHeight() {
+	return height;
+}
 
 }
