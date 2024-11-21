1a8facb2e6afd8c5beef7d65869c312e07d74342
Kelly <k_yee16@u.pacific.edu>
2024-11-20 16:24:03 -0800

    Added zoom function for image in player.java

--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,7 @@ public class Player {
     public static final int RUNSPEED = 3;
     public static final int GRAVITY = 3;
     
-	private GOval player;
+    private GImage playerImage;
 	private Skin skin = new Skin();
 	private int posX;
 	private int posY;
@@ -19,42 +19,46 @@ public class Player {
     	//player = skin.getSkin();
     	//player.move(0, GRAVITY);
     	//for now while we do not have skins yet
-    	player = new GOval(0, 0, 50, 50); // Placeholder rectangle, 50x50
-        player.setFilled(true);
-        player.setColor(java.awt.Color.BLUE); // Use a distinctive color
+    playerImage = new GImage("FrogAvatarDamage#1.png");
+        
+     // Optionally, scale the image if zooming is required
+    double scaleFactor = 5; // Change this value for different zoom levels, but don't go over 5
+    playerImage.setSize(playerImage.getWidth() * scaleFactor, playerImage.getHeight() * scaleFactor);
+        
+    // Set default position
+    playerImage.setLocation(0, 0);
     }
     
-    public GOval getSkin() {
-    	return player;
+    public GImage getSkin() {
+        return playerImage;
     }
     
     public int getPosX() {
-    	return posX;
+        return posX;
     }
     
     public int getPosY() {
-    	return posY;
+        return posY;
     }
     
-    public int setRunDirection(int speed) {
-    	return runDirection + speed;
-    	
+    public void setRunDirection(int speed) {
+        runDirection = speed;
     }
     
-    public int setJumpVelocity(int speed) {
-    	return jumpVelocity + speed;
+    public void setJumpVelocity(int speed) {
+        jumpVelocity = speed;
     }
     
-    public void move_left() {
-    	player.move(-RUNSPEED, 0);
+    public void moveLeft() {
+        playerImage.move(-RUNSPEED, 0);
     }
     
-    public void move_right() {
-    	player.move(RUNSPEED, 0);
+    public void moveRight() {
+        playerImage.move(RUNSPEED, 0);
     }
     
     public void jump() {
-    	player.move(0, -GRAVITY * 2);
+        playerImage.move(0, -GRAVITY * 2);
     }
     
     public void setRow(int row) {
--- src/Skin.java
+++ src/Skin.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 import acm.graphics.*;
 
 public class Skin {
-	private ArrayList<GImage> skinList;
 	private GImage skin;
-	
+
 	public void setSkin() {
 		skin = new GImage("frog.png", 0, 0);
 	}
